be6b1cf8511b5d270459f7c7dc977b36138470c1
Ben Booth <benbooth5@gmail.com>
2011-01-27 09:27:38 -0800

    first commit

---
+++ src/cljfugue/core.clj
@@ -0,0 +1,1 @@
+(ns cljfugue.core)
---
+++ test/cljfugue/test/core.clj
@@ -0,0 +1,6 @@
+(ns cljfugue.test.core
+  (:use [cljfugue.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
