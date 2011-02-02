9f3d087dfbdb757a12c361a9dda2aa2f7f7e51f1
Ben Booth <benbooth5@gmail.com>
2011-02-02 12:20:43 -0800

    rename project

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject cljfugue "1.0.0-SNAPSHOT"
+(defproject clmml "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [org.clojars.mcav/amotoen "0.1.0-SNAPSHOT"]])
